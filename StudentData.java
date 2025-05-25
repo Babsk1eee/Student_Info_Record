15d5a4401949552c688dad41f2ba759b56a56bd2
Babskieee <ronilosepida13@gmail.com>
2025-05-25 08:33:29 +0800

    parameters for the student information

---
+++ StudentData.java
@@ -0,0 +1,42 @@
+public class StudentData {
+
+    String fullName;
+    String course;
+    String section;
+    String studentNo;
+    String sex;
+
+    public StudentData(String fullName, String course, String section, String studentNo, String sex) {
+        this.fullName = fullName;
+        this.course = course;
+        this.section = section;
+        this.studentNo = studentNo;
+        this.sex = sex;
+    }
+
+
+    public String getFullName(){
+        return fullName;
+    }
+
+    public String getCourse(){
+        return course;
+    }
+
+    public String getSection(){
+        return section;
+    }
+
+    public String getStudentNo() {
+        return studentNo;
+    }
+
+    public String getSex(){
+        return sex;
+    }
+    
+
+    public String toString() {
+        return studentNo + " | " + fullName + " | " + " | " + sex + " | " + course + " | " + section;
+    }
+}
